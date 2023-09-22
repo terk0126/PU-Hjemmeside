8474383cff260a07fb615dc9393a6ca7bfbc0bf9
Terkel Iversen <Ttiversen06@gmail.com>
2023-09-22 21:40:41 +0200

    PU - 22/09/2023 nummer + multiple Coice spørgsmål

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let questions = []; // Initialize an empty array to store questions.
+let currentQuestion; // Declare currentQuestion as a global variable
+let recentlyAskedQuestions = []; // Initialize an array to store recently asked questions.
+
+fetchQuestions(); // Call the fetchQuestions function to load questions when the page loads.
 
 // Function to fetch questions from the JSON file.
 function fetchQuestions() {
@@ -13,10 +17,8 @@ function fetchQuestions() {
         });
 }
 
-fetchQuestions(); // Call the fetchQuestions function to load questions when the page loads.
 
-
-let currentQuestionIndex = 0;
+var currentQuestionIndex = 0;
 let score = 0;
 let points = 0;
 
@@ -24,17 +26,69 @@ const questionContainer = document.getElementById("question-container");
 const nextButton = document.getElementById("next-button");
 const resultContainer = document.getElementById("result-container");
 
-function displayQuestion() {
-    const currentQuestion = questions[currentQuestionIndex];
-    const optionsHTML = currentQuestion.options.map(option =>
-        `<label><input type="radio" name="option" value="${option[0]}"> ${option[0]}</label><br>`
-    ).join("");
-
-    questionContainer.innerHTML = `
-        <p>${currentQuestion.question}</p>
-        ${optionsHTML}
-    `;
+function getRandomQuestion() {
+    let randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * questions.length);
+    } while (recentlyAskedQuestions.includes(randomIndex) && recentlyAskedQuestions.length < 10);
+    recentlyAskedQuestions.push(randomIndex);
+    if (recentlyAskedQuestions.length > 10) {
+        recentlyAskedQuestions.shift(); // Remove the oldest question from the list.
+    }
+    console.log(randomIndex);
+    console.log(recentlyAskedQuestions);
+    return questions[randomIndex];
 }
+
+function displayQuestion() {
+    currentQuestion = getRandomQuestion(); // Assign the global currentQuestion variable
+    console.log(currentQuestion);
+
+    if (!currentQuestion) {
+        console.error('No currentQuestion found.');
+        return;
+    }
+
+    console.log(currentQuestion.type);
+
+    if (currentQuestion.type === "multipleChoice") {
+        if (!currentQuestion.options || !Array.isArray(currentQuestion.options)) {
+            console.error('Invalid options for multipleChoice question.');
+            return;
+        }
+
+        const optionsHTML = currentQuestion.options.map(option =>
+            `<label><input type="radio" name="option" value="${option[0]}"> ${option[0]}</label><br>`
+        ).join("");
+
+        questionContainer.innerHTML = `
+            <p>${currentQuestion.question}</p>
+            ${optionsHTML}
+        `;
+
+    } else if (currentQuestion.type === "number") {
+        if (currentQuestion.options || Array.isArray(currentQuestion.options)) {
+            console.error('Invalid options for number question.');
+            return;
+        }
+
+        var questionText = currentQuestion.question
+
+        if (currentQuestion.marginOfError !== undefined) {
+            questionText += ` (Inden for ${currentQuestion.marginOfError})`;
+        }
+
+        const questionHTML = `<input type="number" name="numberAnswer" id="numberAnswer" placeholder="Skriv dit svar her">`
+
+        
+
+        questionContainer.innerHTML = `
+            <p>${questionText}</p>
+            ${questionHTML}
+        `;
+    }
+}
+
 
 function updateScoreAndPoints() {
     const scoreContainer = document.getElementById("score");
@@ -45,33 +99,50 @@ function updateScoreAndPoints() {
 }
 
 function checkAnswer() {
-    const selectedOption = document.querySelector('input[name="option"]:checked');
-
-    if (!selectedOption) {
-        return; // User hasn't selected an option
+    if (!currentQuestion) {
+        console.error('No currentQuestion found.');
+        return;
     }
 
-    const userAnswer = selectedOption.value;
-    const correctAnswer = questions[currentQuestionIndex].correctAnswer;
+    if (currentQuestion.type === "multipleChoice") {
+        var selectedOption = document.querySelector('input[name="option"]:checked');
+        if (!selectedOption) { return; } // Added null check
 
-    // Find the selected option's text and points
-    const selectedOptionData = questions[currentQuestionIndex].options.find(optionData => optionData[0] === userAnswer);
+        const userAnswer = selectedOption.value;
+        const correctAnswer = currentQuestion.correctAnswer; // Use the global currentQuestion
 
-    if (selectedOptionData) {
-        let answerPoints = selectedOptionData[1]; // Option points
+        // Find the selected option's text and points
+        const selectedOptionData = currentQuestion.options.find(optionData => optionData[0] === userAnswer);
 
-        if (userAnswer === correctAnswer) {
-            score++;
-            points += answerPoints;
+        if (selectedOptionData) {
+            let answerPoints = selectedOptionData[1]; // Option points
+
+            if (userAnswer === correctAnswer) {
+                score++;
+                points += answerPoints;
+            }
         }
+
+    } else if (currentQuestion.type === "number") {
+        var numberInput = document.querySelector('input[name="numberAnswer"]');
+        if (!numberInput) { return; } // Added null check
+
+        const userAnswer = parseFloat(numberInput.value);
+        const correctAnswer = parseFloat(currentQuestion.correctAnswer); // Use the global currentQuestion
+        const marginOfError = parseFloat(currentQuestion.marginOfError) || 0; // Default to 0 if marginOfError is not specified
+
+        if (correctAnswer !== undefined && userAnswer !== undefined) {
+            if (Math.abs(userAnswer - correctAnswer) <= marginOfError) {
+                // User's answer is within the acceptable margin of error
+                score++;
+                points += currentQuestion.point;
+            }
+        }
+
     }
-
     
-
-    
-
     currentQuestionIndex++;
-    updateScoreAndPoints()
+    updateScoreAndPoints();
 
     if (currentQuestionIndex < questions.length) {
         displayQuestion();
@@ -80,24 +151,19 @@ function checkAnswer() {
     }
 }
 
-
 function showResult() {
-
-
-    let maxPossiblePoints;
-    questions.forEach(question => {
-        console.log(question.options);
-        var correctOption = question.options.find(maxPointOption => {maxPointOption[0] === question.correctAnswer});
-        console.log(correctOption);
-        maxPossiblePoints += correctOption[1] 
-    });
-
-
-
     questionContainer.innerHTML = "";
     nextButton.style.display = "none";
-    resultContainer.innerHTML = `You scored ${score} out of ${questions.length}! \n And got ${points} out of ${maxPossiblePoints} points`;
-};
+    resultContainer.innerHTML = `You scored ${score} out of ${questions.length}! \n And got ${points} out of  points`;
+}
 
 nextButton.addEventListener("click", checkAnswer);
 displayQuestion(); // Display the first question
+
+function getRandomItem(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    // get random item
+    const item = arr[randomIndex];
+    return item;
+}
