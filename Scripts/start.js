33e683cf6c00dd986e4e8f9bb5fc93d254013a1b
Terkel Iversen <Ttiversen06@gmail.com>
2023-09-30 18:54:29 +0200

    tilføjede menuFag.html

--- Scripts/start.js
+++ Scripts/start.js
@@ -2,18 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // start.js
 const endlessModeButton = document.getElementById("endless-mode-button");
-const timedModeButton = document.getElementById("timed-mode-button");
+const timedModeButton = document.getElementById("timed-menu-button");
 
 // Event listeners for mode selection
 endlessModeButton.addEventListener("click", () => {
     // Redirect to the quiz page for endless mode'
-    console.log("HEJ");
     window.location.href = "endless.html";
 });
 
 timedModeButton.addEventListener("click", () => {
-    // Redirect to the quiz page for timed mode
-    window.location.href = "quiz.html";
+    // Redirect to the menu page for timed mode
+    window.location.href = "menuFag.html";
 });
 
 
